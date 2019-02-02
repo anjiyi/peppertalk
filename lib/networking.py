a06f5023495c6ff0c48601f33e91bbf93682d953
Andre Quispesaravia <andreq@fb.com>
2019-02-01 23:24:46 -0800

    [Debug] add debugging information for busted messenger replies

--- lib/networking.py
+++ lib/networking.py
@@ -11,15 +11,15 @@ def send_reply(reply):
     Sending response back to the user using facebook graph API
     """
     PAT = os.environ["PAT"]
-    json = {"recipient": {"id": reply.sender_id}, "message": reply.build_message()}
+    messenger_data = {"recipient": {"id": reply.sender_id}, "message": reply.build_message()}
     r = requests.post(
         "https://graph.facebook.com/v2.6/me/messages",
         params={"access_token": PAT},
         headers={"Content-Type": "application/json"},
-        data=json.dumps(json),
+        data=json.dumps(messenger_data),
     )
 
     if r.status_code != 200:
         print(reply)
-        print(json)
+        print(messenger_data)
         print(r.json())
